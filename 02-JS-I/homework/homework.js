64e7cd1cd0f41e60a1dc3855f0524ccce9db39a3
Rikardo <ricardo.zedano@gmail.com>
2022-07-02 00:51:28 -0500

    5 ejercicios resueltos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -10,10 +10,12 @@ const nuevoNum = 8;
 const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
+console.log(nuevaResta);
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40;
+console.log(nuevaMultiplicacion);
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
